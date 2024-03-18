6e05ba05bd6f0f59b882a3abf6372195e50ecb21
User <User@WIN-RCUACGN9TIQ>
2024-03-18 19:23:43 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Class1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+class Hero()
+{
+    private readonly int color;
+    public int Color
+    {
+        get
+        {
+            return color;
+        }
+        init
+        {
+            color = value;
+        }
+    }
+    private readonly string name;
+    public string Name
+    {
+        get
+        {
+            return name;
+        }
+        init
+        {
+            name = value;
+        }
+    }
+    internal int maxHealth;
+    public int MaxHealth
+    {
+        get
+        {
+            return maxHealth;
+        }
+        set
+        {
+            maxHealth = value;
+        }
+    }
+    private int health;
+    public int Health
+    {
+        get
+        {
+            return health;
+        }
+        set
+        {
+            health = value;
+            if (health > MaxHealth) health = MaxHealth;
+        }
+    }
+    internal int maxForce;
+    public int MaxForce
+    {
+        get
+        {
+            return maxForce;
+        }
+        set
+        {
+            maxForce = value;
+        }
+    }
+    private int force;
+    public int Force
+    {
+        get
+        {
+            return force;
+        }
+        set
+        {
+            force = value;
+            if (force > MaxForce) force = MaxForce;
+            if (force < 0) force = 0;
+        }
+    }
+    public Hero(string Name, int MaxHealth, int MaxForce, int Color) : this()
+    {
+        this.Name = Name;
+        this.MaxHealth = MaxHealth;
+        this.MaxForce = MaxForce;
+        this.Color = Color;
+        Health = MaxHealth;
+        Force = MaxForce;
+    }
+
+    public string Fight(Hero enemy)
+    {
+        Random rnd = new();
+        int Attack = Force + rnd.Next(1, 10);
+        Force -= enemy.Health / 2;
+        enemy.Health -= Attack;
+        string outcome = $"Герой {Name} наносит удар по врагу {enemy.Name} на {Attack} урона\n";
+        if (enemy.Health <= 0)
+        {
+            outcome += $"Враг сокрушён!\nУровень повышен!";
+            LevelUp();
+        }
+        if (enemy.Health > 0)
+        {
+            Attack = enemy.Force;
+            Health -= Attack;
+            outcome += $"Враг оказался сильнее! Герой {Name} получает {Attack} урона!";
+        }
+        return outcome;
+    }
+    private void LevelUp()
+    {
+        MaxHealth += 50;
+        MaxForce += 25;
+    }
+    public string Heal()
+    {
+        if (Health == MaxHealth)
+        {
+            return "Здоровье достигло максимума!";
+        }
+        Health += Force;
+        int HealValue = Force;
+        Force /= 2;
+        return $"Герой {Name} исцеляет себя на {HealValue}";
+    }
+    public string Wait()
+    {
+        if (Force == MaxForce)
+        {
+            return "Сила достигла максимума!";
+        }
+        Force += (MaxForce / 4);
+        return $"Герой {Name} отдохнул. Запас сил восстановлен на {MaxForce/4}.";
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Напишите имя своего героя: ");
+
+        Hero hero = new Hero(Console.ReadLine(), 100, 100, (int)ConsoleColor.Green);
+
+        List<Hero> enemies = new List<Hero>()
+            {
+                new Hero("дикий пёс", 20, 40, (int)ConsoleColor.DarkGray),
+                new Hero("гоблин", 30, 30, (int)ConsoleColor.DarkGreen),
+                new Hero("Бандит", 50, 45, (int)ConsoleColor.DarkBlue),
+                new Hero("бесёнок", 65, 30, (int)ConsoleColor.Red),
+                new Hero("Скелет", 75, 40, (int)ConsoleColor.White),
+                new Hero("Демон", 250, 120, (int)ConsoleColor.DarkRed),
+                new Hero("Скелет-рыцарь", 300, 100, (int)ConsoleColor.Gray),
+                new Hero("Тёмный паладин", 500, 150, (int)ConsoleColor.Green),
+            };
+
+        int days = 0;
+
+        while (true)
+        {
+            days++;
+            Console.Clear();
+            if (hero.Health <= 0)
+            {
+                Console.WriteLine(hero.Name + " погиб!");
+                break;
+            }
+            if (enemies.Count == 0)
+            {
+                Console.WriteLine("Все враги побеждены!");
+                break;
+            }
+
+            Console.WriteLine("День: " + days);
+            Console.WriteLine();
+            PrintHeroInfo(hero);
+            Console.WriteLine();
+            PrintHeroInfo(enemies[0]);
+            Console.WriteLine();
+            HeroMenu(hero, enemies[0]);
+
+            if (enemies[0].Health <= 0)
+            {
+                enemies.RemoveAt(0);
+            }
+        }
+
+        Console.WriteLine("Дней прожито: " + days);
+        Console.ReadKey();
+    }
+
+    static void PrintHeroInfo(Hero hero)
+    {
+        Console.ForegroundColor = (ConsoleColor)hero.Color;
+        Console.WriteLine(hero.Name);
+        Console.WriteLine("Здоровье: " + hero.Health + "/" + hero.MaxHealth);
+        Console.WriteLine("Силы: " + hero.Force + "/" + hero.MaxForce);
+        Console.ResetColor();
+    }
+
+    static void HeroMenu(Hero hero, Hero enemy)
+    {
+        Console.WriteLine("==========");
+        Console.WriteLine("F - сражение с врагом");
+        Console.WriteLine("H - лечение");
+        Console.WriteLine("W - восстановить силы");
+
+        ConsoleKey key = Console.ReadKey().Key;
+
+        Console.Clear();
+
+        switch (key)
+        {
+            case ConsoleKey.F:
+                Console.WriteLine(hero.Fight(enemy));
+                break;
+            case ConsoleKey.H:
+                Console.WriteLine(hero.Heal());
+                break;
+            case ConsoleKey.W:
+                Console.WriteLine(hero.Wait());
+                break;
+        }
+
+        Console.ReadKey();
+    }
+}
+
